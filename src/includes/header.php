61a4394b681b347c60ed555529080fbe7930b0b7
Daniel Litzbach <daniel.litzbach@com-sys.de>
2015-01-21 20:07:47 +0100

    added some base files

---
+++ src/includes/header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<link rel="stylesheet" href="../style/style.css">
+<title>CryptoManager</title>
+</head>
+<body>
+<table width="100%" border="0" cellspacing="0">
+<tr class="tableheader">
+<td colspan="3">&nbsp;</td>
+</tr><tr>
+<td align="center">
+<div class="middle">CryptoManager</div>
+</td>
+</tr>
+<tr class="tableheader">
+<td colspan="3">&nbsp;</td>
+</tr>
+</table>
+<br>
