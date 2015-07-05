737169ed5aad2482bdbd057da99c83cabcefb210
Daniel Litzbach <info@litzinetz.de>
2015-07-05 15:23:43 +0200

    * secured helpers-forder added, will be used for bash or perl scripts that are run from the console in background * added ability so save the vpn credentials in the tc-container so we can authenticate non-interactive

--- src/libs/crypto.php
+++ src/libs/crypto.php
@@ -35,6 +35,7 @@ class crypto
 	
 	public function mount_container($pwd,$cont_name)
 	{
+		if($this->container_mounted()) $this->dismount_container();
 		$cont_path=VPN_CONF_ENC.$this->cleanup_filename($cont_name);
 		$runcmd='sudo '.TC_BIN.' --non-interactive '.$cont_path.' '.VPN_CONF_MNT.' -p "'.$pwd.'"';
 		
@@ -80,6 +81,25 @@ class crypto
 			}
 		}
 		return false;
+	}
+	
+	public function AddCredentials($username,$password)
+	{
+		if(!$this->container_mounted())
+		{
+			return false;
+		}
+		file_put_contents(VPN_CONF_MNT.'cred.dat',$username."\n".$password) or die('Error writing cred.dat');
+		
+		// TODO: Read list of ovpn files into array $ovpn_list. For testing purposes, we make it static
+		$ovpn_list=array('/crypi/mnt/testvpn/testvpn.ovpn');
+		
+		foreach($ovpn_list as $ovpn)
+		{
+			$fh=fopen($ovpn,'a');
+			fwrite($fh,"auth-user-pass ".VPN_CONF_MNT."cred.dat");
+			fclose($fh);
+		}
 	}
 }
 ?>
