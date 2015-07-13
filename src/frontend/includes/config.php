29aec0f4d7bc401b79b26b133ffc96cd0db4a687
Daniel Litzbach <dl@litzinetz.de>
2015-07-13 12:49:55 +0200

    * Added config options * Added init script for setting ip parameters

--- src/frontend/includes/config.php
+++ src/frontend/includes/config.php
@@ -8,10 +8,11 @@ define('TC_BIN','/usr/bin/truecrypt');
 define('OVPN_BIN','/usr/sbin/openvpn');
 
 define('VPN_CONF_BASEDIR','/crypi/');
-define('BACKENDPATH','/backend/');
 
 /////
 
 define('VPN_CONF_ENC',VPN_CONF_BASEDIR.'enc/');
 define('VPN_CONF_MNT',VPN_CONF_BASEDIR.'mnt/');
+define('BACKENDPATH',VPN_CONF_BASEDIR.'backend/');
+define('DATAPATH',VPN_CONF_BASEDIR.'data/');
 ?>
