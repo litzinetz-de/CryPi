b25c6f6d923617f33dfc01a5bd7ec0359b0f1e01
Daniel Litzbach <dl@litzinetz.de>
2015-12-05 12:47:51 +0100

    * Moved setup to own script, can be updated and then called by the installer

--- src/setup/setup_backend.sh
+++ src/setup/setup_backend.sh
@@ -7,21 +7,21 @@ fresh_install_generic()
   echo -e "\033[0;33mI will now install some software and upgrade the system."
   echo -e "Hit enter to continue.\033[0;37m"
   read
-  apt-get update
-  apt-get upgrade
-  apt-get install apache2 git
+  apt-get install apache2
   wwwdir=$(apachectl -S 2> /dev/null | grep "Main DocumentRoot" | sed -e 's/\<Main DocumentRoot\>//g' | sed s/://g | sed 's/ //g')
   echo -e "\033[0;33mDone. I will now start installing CryptoPi."
   echo -e "Hit enter to continue.\033[0;37m"
   read
+  echo "Clearing up wwwdir ($wwwdir)"
   rm -rf $wwwdir/*
-  mkdir crypi_repo
-  git clone https://github.com/litzinetz-de/CryPi.git crypi_repo
+  echo "Installing web frontend"
   cp -R crypi_repo/src/frontend/* $wwwdir
   chown -R www-data:www-data $wwwdir
+  echo "Installing init scripts"
   cp crypi_repo/src/init-scripts/crypi_init /etc/init.d/
   chmod ugo+x /etc/init.d/crypi_init
   update-rc.d crypi_init defaults
+  echo "Creating crypi directories"
   mkdir /crypi
   mkdir /crypi/data
   mkdir /crypi/enc
@@ -29,6 +29,15 @@ fresh_install_generic()
   mkdir /crypi/scripts
   mkdir /crypi/upload_tmp
   mkdir /crypi/upload_workdir
+  echo "Installing crypi scripts"
+  cp crypi_repo/src/scripts/* /crypi/scripts
+  chown -R www-data:www-data /crypi
+  chmod u+x /crypi/scripts/*
+  echo "Creating default config files"
+  echo "false" > /crypi/data/bypass_vpn.dat
+  echo "192.168.0.254" > /crypi/data/networking_addr.dat
+  echo "255.255.255.0" > /crypi/data/networking_mask.dat
+  echo "192.168.0.1" > /crypi/data/networking_gateway.dat
 }
 
 ########################
